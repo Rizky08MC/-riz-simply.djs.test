522cefe35cfbdc7b34849c7fdb29d760adbeb6f0
Rizky08MC <85172211+Rizky08MC@users.noreply.github.com>
2021-09-04 12:52:19 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const { ClientLogin } = require('./function/ClientLogin');
+const { ClientMessage } = require('./function/ClientMessage');
+const { ClientHandler } = require('./function/ClientHandler');
+const { ClientCreate } = require('./,function/ClientCreate');
+
+module.exports = { ClientCreate, ClientLogin, ClientMessage, ClientHandler };
